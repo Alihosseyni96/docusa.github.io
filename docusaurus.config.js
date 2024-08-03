25bd53e35e16dcdc3d79c9f25bdaff22cb1c482e
Pourya <pourya.lh96@gmail.com>
2024-08-03 16:03:07 +0330

    change deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Alihosseyni96', // Usually your GitHub org/user name.
   projectName: 'docusa.github.io ', // Usually your repo name.
-  deploymentBranch:"gh-pages",
+ 
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
